5758c0786a11cd1187127fee6bc556e25a0d2bc4
bakhodirsbox <bahodir9293@gmail.com>
2018-03-18 18:50:50 -0500

    added gui

---
+++ src/bs/framework/Customer.java
@@ -0,0 +1,4 @@
+package bs.framework;
+
+public abstract class Customer {
+}
---
+++ src/bs/framework/IFinco.java
@@ -0,0 +1,14 @@
+package bs.framework;
+
+public interface IFinco {
+
+    void addInterest();
+
+    void createCustomer(Customer customer);
+
+    void deposit();
+
+    void withdraw();
+
+    void accountReport();
+}
