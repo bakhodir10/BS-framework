9a1d683b3f7b1db2a768a6a34340cdcdc4aba9f3
tanianipa <taniashermeen@gmail.com>
2018-03-19 22:18:28 -0500

    Update Company.java
    added few things

--- src/bs/framework/Company.java
+++ src/bs/framework/Company.java
@@ -1,5 +1,22 @@
 package bs.framework;
 
 public class Company extends Customer {
-    private int numOfEmployees;
+	private int numOfEmployees;
+
+	public Company() {
+		super();
+
+	}
+
+	public Company(String name, String street, String city, String state, String zip, String email) {
+		super(name, street, city, state, zip, email);
+	}
+
+	public int getNumOfEmployees() {
+		return numOfEmployees;
+	}
+
+	public void setNumOfEmployees(int numOfEmployees) {
+		this.numOfEmployees = numOfEmployees;
+	}
 }
