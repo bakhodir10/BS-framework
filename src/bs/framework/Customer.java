d75b0b2aa38ec7239dc0d6ec79095712bf03a899
tanianipa <taniashermeen@gmail.com>
2018-03-19 22:30:08 -0500

    Update Customer.java
    added few things

--- src/bs/framework/Customer.java
+++ src/bs/framework/Customer.java
@@ -15,22 +15,84 @@ public class Customer implements ICustomer {
     public Customer() {
         this.accountList = new LinkedList<>();
     }
+public Customer(String name, String street, String city, String state, String zip, String email) {
+		super();
+		this.name = name;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.email = email;
+		this.accountList = new LinkedList<>();
+	}
 
-    @Override
-    public void addAccount(IAccount account) {
-        this.accountList.add(account);
-    }
+	// Tania
+	@Override
+	public void addAccount(IAccount account) {
+		account.setCustomer(this);
+		this.accountList.add(account);
+	}
 
-    @Override
-    public void removeAccount(IAccount account) {
-        this.accountList.remove(account);
-    }
+	@Override
+	public void removeAccount(IAccount account) {
+		this.accountList.remove(account);
+	}
 
-    @Override
-    public void sendEmailCustomer() {
+	@Override
+	public void sendEmailCustomer() {
 
-    }
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+    
+   
+  
     @Override
     public List<IAccount> getAccount() {
         return this.accountList;
@@ -43,7 +105,5 @@ public class Customer implements ICustomer {
         return this.email.equals(c.getEmail());
     }
 
-    public String getEmail() {
-        return email;
-    }
+ 
 }
