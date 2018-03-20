84b4c480a2111bc06191bb9d1001af76ecc9bf7f
tanianipa <taniashermeen@gmail.com>
2018-03-19 22:22:17 -0500

    Update CustomerFactory.java
    added parameters for initialization

--- src/bs/framework/CustomerFactory.java
+++ src/bs/framework/CustomerFactory.java
@@ -1,14 +1,15 @@
 package bs.framework;
 
 public class CustomerFactory {
-    public static ICustomer getCustomer(CustomerType type) {
-        switch (type) {
-            case PERSON:
-                return new Person();
-            case COMPANY:
-                return new Company();
-            default:
-                return null;
-        }
-    }
+    public static ICustomer getCustomer(CustomerType type, String name, String street, String city, String state,
+			String zip, String email, Date bDate) {
+		switch (type) {
+		case PERSON:
+			return new Person(name, street, city, state, zip, email, bDate);
+		case COMPANY:
+			return new Company(name, street, city, state, zip, email);
+		default:
+			return null;
+		}
+	}
 }
