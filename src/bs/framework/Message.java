ad190ab73bc8fd74893ecd6f5539c6e138d49292
bakhodirsbox <bahodir9293@gmail.com>
2018-03-21 10:27:56 -0500

    we wanna sleep ))

--- src/bs/framework/Message.java
+++ src/bs/framework/Message.java
@@ -4,8 +4,6 @@ public class Message implements IMessage {
 
     @Override
     public void sendMessage(IAccount account) {
-//        account.getCustomer().getEmail()
-        System.out.println("The message ");
-        // todo send message to the account owner
+        System.out.println("A message has been sent to " + account.getCustomer().getName());
     }
 }
